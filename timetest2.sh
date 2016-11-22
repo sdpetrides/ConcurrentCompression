0b351fd014c67674ce9095fbb946b1203fffca4a
Stephen Petrides <sdpetrides@gmail.com>
2016-11-22 12:17:26 -0500

    Added timetest2.sh

---
+++ timetest2.sh
@@ -0,0 +1,96 @@
+echo Threading 1
+time ./compressT_LOLS ./largefile.txt 1
+time ./compressT_LOLS ./largefile.txt 1
+time ./compressT_LOLS ./largefile.txt 1
+time ./compressT_LOLS ./largefile.txt 1
+time ./compressT_LOLS ./largefile.txt 1
+echo Threading 2
+time ./compressT_LOLS ./largefile.txt 2
+time ./compressT_LOLS ./largefile.txt 2
+time ./compressT_LOLS ./largefile.txt 2
+time ./compressT_LOLS ./largefile.txt 2
+time ./compressT_LOLS ./largefile.txt 2
+echo Threading 4
+time ./compressT_LOLS ./largefile.txt 4
+time ./compressT_LOLS ./largefile.txt 4
+time ./compressT_LOLS ./largefile.txt 4
+time ./compressT_LOLS ./largefile.txt 4
+time ./compressT_LOLS ./largefile.txt 4
+echo Threading 8
+time ./compressT_LOLS ./largefile.txt 8
+time ./compressT_LOLS ./largefile.txt 8
+time ./compressT_LOLS ./largefile.txt 8
+time ./compressT_LOLS ./largefile.txt 8
+time ./compressT_LOLS ./largefile.txt 8
+echo Threading 16
+time ./compressT_LOLS ./largefile.txt 16
+time ./compressT_LOLS ./largefile.txt 16
+time ./compressT_LOLS ./largefile.txt 16
+time ./compressT_LOLS ./largefile.txt 16
+time ./compressT_LOLS ./largefile.txt 16
+echo Threading 32
+time ./compressT_LOLS ./largefile.txt 32
+time ./compressT_LOLS ./largefile.txt 32
+time ./compressT_LOLS ./largefile.txt 32
+time ./compressT_LOLS ./largefile.txt 32
+time ./compressT_LOLS ./largefile.txt 32
+echo Threading 64
+time ./compressT_LOLS ./largefile.txt 64
+time ./compressT_LOLS ./largefile.txt 64
+time ./compressT_LOLS ./largefile.txt 64
+time ./compressT_LOLS ./largefile.txt 64
+time ./compressT_LOLS ./largefile.txt 64
+echo Threading 128
+time ./compressT_LOLS ./largefile.txt 128
+time ./compressT_LOLS ./largefile.txt 128
+time ./compressT_LOLS ./largefile.txt 128
+time ./compressT_LOLS ./largefile.txt 128
+time ./compressT_LOLS ./largefile.txt 128
+echo Processing 1
+time ./compressR_LOLS ./largefile.txt 1
+time ./compressR_LOLS ./largefile.txt 1
+time ./compressR_LOLS ./largefile.txt 1
+time ./compressR_LOLS ./largefile.txt 1
+time ./compressR_LOLS ./largefile.txt 1
+echo Processing 2
+time ./compressR_LOLS ./largefile.txt 2
+time ./compressR_LOLS ./largefile.txt 2
+time ./compressR_LOLS ./largefile.txt 2
+time ./compressR_LOLS ./largefile.txt 2
+time ./compressR_LOLS ./largefile.txt 2
+echo Processing 4
+time ./compressR_LOLS ./largefile.txt 4
+time ./compressR_LOLS ./largefile.txt 4
+time ./compressR_LOLS ./largefile.txt 4
+time ./compressR_LOLS ./largefile.txt 4
+time ./compressR_LOLS ./largefile.txt 4
+echo Processing 8
+time ./compressR_LOLS ./largefile.txt 8
+time ./compressR_LOLS ./largefile.txt 8
+time ./compressR_LOLS ./largefile.txt 8
+time ./compressR_LOLS ./largefile.txt 8
+time ./compressR_LOLS ./largefile.txt 8
+echo Processing 16
+time ./compressR_LOLS ./largefile.txt 16
+time ./compressR_LOLS ./largefile.txt 16
+time ./compressR_LOLS ./largefile.txt 16
+time ./compressR_LOLS ./largefile.txt 16
+time ./compressR_LOLS ./largefile.txt 16
+echo Processing 32
+time ./compressR_LOLS ./largefile.txt 32
+time ./compressR_LOLS ./largefile.txt 32
+time ./compressR_LOLS ./largefile.txt 32
+time ./compressR_LOLS ./largefile.txt 32
+time ./compressR_LOLS ./largefile.txt 32
+echo Processing 64
+time ./compressR_LOLS ./largefile.txt 64
+time ./compressR_LOLS ./largefile.txt 64
+time ./compressR_LOLS ./largefile.txt 64
+time ./compressR_LOLS ./largefile.txt 64
+time ./compressR_LOLS ./largefile.txt 64
+echo Processing 128
+time ./compressR_LOLS ./largefile.txt 128
+time ./compressR_LOLS ./largefile.txt 128
+time ./compressR_LOLS ./largefile.txt 128
+time ./compressR_LOLS ./largefile.txt 128
+time ./compressR_LOLS ./largefile.txt 128
